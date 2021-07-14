a3397a84938698c39dfe5c46ac59d37b2a0b4f7b
bulbabyte <doylemariah18@gmail.com>
2021-07-13 19:57:13 -0500

    Update 2D max and min.cpp

--- Array-Vectors/Two-Dimensional Arrays/2D max and min.cpp
+++ Array-Vectors/Two-Dimensional Arrays/2D max and min.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+/* Find the maximum value and minimum value in milesTracker. 
+Assign the maximum value to maxMiles, and the minimum value to minMiles. 
+Sample output for the given program:
+Min miles: -10
+Max miles: 40
+*/
+
 int main() {
    const int NUM_ROWS = 2;
    const int NUM_COLS = 2;
