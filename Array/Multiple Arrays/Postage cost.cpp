7634c56769be92b52fdea2ce3facb3a8f0f397b4
bulbabyte <doylemariah18@gmail.com>
2021-07-17 16:30:27 -0500

    Update Postage cost.cpp

--- Array/Multiple Arrays/Postage cost.cpp
+++ Array/Multiple Arrays/Postage cost.cpp
@@ -22,7 +22,7 @@ int main () {
     letterWeights[i] = 3.5;  postageCosts[i] = 106; ++i;
     letterWeights[i] = 4;    postageCosts[i] = 152; ++i;
     letterWeights[i] = 5;    postageCosts[i] = 172; ++i;
-    letterWeights[i] = 6;    postageCosts[i] = 192; ++i;
+    letterWeights[i] = 6;    postageCosts[i] = 192; ++i;            // uses [i] instead of [0], [1], etc because its less prone to errors and enables easy reordering or inserting of new weights and costs
     letterWeights[i] = 7;    postageCosts[i] = 212; ++i;
     letterWeights[i] = 8;    postageCosts[i] = 232; ++i;
     letterWeights[i] = 9;    postageCosts[i] = 252; ++i;
@@ -40,8 +40,8 @@ int main () {
     */
    foundWeight = false;
 
-   for (i = 0; (i < NUM_ELEMENTS) && (!foundWeight); ++i) {
-       if (userLetterWeight <= letterWeights[i]) {
+   for (i = 0; (i < NUM_ELEMENTS) && (!foundWeight); ++i) {     // prevents the loop from going through entire array once correct wieght is found + Ommitting the check for found would result in incorrect output; would print the postage costs for all letter weights > user's weight
+       if (userLetterWeight <= letterWeights[i]) {              // compares user-entered letter weights w/ current element in letterWeight array
            foundWeight = true;
            cout << "Postage for USPS first class mail is ";
            cout << postageCosts[i] << " cents" << endl;
