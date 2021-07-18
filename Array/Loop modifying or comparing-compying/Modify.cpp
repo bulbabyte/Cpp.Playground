b925d194662836563df699be5ecc92f49b1c95f0
bulbabyte <doylemariah18@gmail.com>
2021-07-17 19:38:32 -0500

    Update Modify.cpp

--- Array/Loop modifying or comparing-compying/Modify.cpp
+++ Array/Loop modifying or comparing-compying/Modify.cpp
@@ -7,5 +7,20 @@ Scores after the loop: 30, 50, 70, 40
 The first element is 30 or 10+20, the second element is 50 or 20+30 and third is 70 or 30+40
 */
 
+#include <iostream>
+using namespace std;
+
+int main() {
+   const int SCORES_SIZE = 4;
+   int bonusScores[SCORES_SIZE];
+   int i = 0;
+
+   bonusScores[0] = 10;
+   bonusScores[1] = 20;
+   bonusScores[2] = 30;
+   bonusScores[3] = 40;
+
+   /* Your solution */
+
 
 
