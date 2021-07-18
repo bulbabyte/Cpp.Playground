162d90f166a4a0949f282366129092d318f5eab2
bulbabyte <doylemariah18@gmail.com>
2021-07-17 19:37:49 -0500

    Create Modify.cpp

---
+++ Array/Loop modifying or comparing-compying/Modify.cpp
@@ -0,0 +1,11 @@
+/*
+Write a loop that sets each array element to the sum of itself and the next element,
+except for the last element which stays the same. Be careful not to index beyond last element, Ex:
+Initial scores:         10, 20, 30, 40
+Scores after the loop: 30, 50, 70, 40
+
+The first element is 30 or 10+20, the second element is 50 or 20+30 and third is 70 or 30+40
+*/
+
+
+
