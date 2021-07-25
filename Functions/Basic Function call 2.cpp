1f191e8415dbe1a44cf46005f86c72e8245a69a0
bulbabyte <doylemariah18@gmail.com>
2021-07-25 10:33:15 -0500

    Create Basic Function call 2.cpp

---
+++ Functions/Basic Function call 2.cpp
@@ -0,0 +1,28 @@
+/* Complete the PrintShape() function to print the following shape
+End with newline Ex:
+***
+***
+***
+*/
+
+#include <iostream>
+using namespace std;
+
+void PrintShape() {
+
+    // solution here
+    
+    char shapeChar = '*';
+
+    cout << shapeChar << shapeChar << shapeChar << endl;
+    cout << shapeChar << shapeChar << shapeChar << endl;
+    cout << shapeChar << shapeChar << shapeChar << endl;
+
+    return;
+}
+
+int main() {
+    PrintShape();
+
+    return 0;
+}
