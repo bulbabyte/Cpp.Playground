5f14069d29480e2c5a7f2d65cfc9863d84a3a60e
bulbabyte <doylemariah18@gmail.com>
2021-07-31 17:42:24 -0500

    Update reverse.cpp

--- Vectors/Swapping Vectors/reverse.cpp
+++ Vectors/Swapping Vectors/reverse.cpp
@@ -8,31 +8,35 @@ using namespace std;
     to: 80 70 60 50 40 30 20 10
 */
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
 int main() {
-    const int NUM_ELEMENTS = 8;         // Number of elements
-    vector<int> revVctr (NUM_ELEMENTS)  // User values
-    unsigned int i;                     // Loop index
-    int tmpValue;                       // Placeholder
-    cout << "Enter " << NUM_ELEMENTS << " integer values . . ." << endl;
-    for ( i = 0; i < revVctr,size(); ++i) {
-        cout << "Value: ";
-        cin >> revVctr.at(i);
-    }
-
-    // Reverse
-    for(i = 0; i < (revVctr.size() / 2); ++i){               // reversal is completed halfway through interations
-        tmpValue = revVctr.at(i);                            // These 3 statements swap 
-        revVctr.at(i) = revVctr.at(revVctr.size() - 1 - i));
-        revVctr.at(revVctr.size() - 1 - i) = tmpValue;      //Swap
-    }
-
-
-    // Print values
-    cout << endl << "New values: ";
-    for( i = 0; i < revVctr.size(); ++i){
-        cout << " " << revVctr.at(i);
-    }
-    cout << endl;
-
-    return 0;
+   const int NUM_ELEMENTS = 8;        // Number of elements
+   vector<int> revVctr(NUM_ELEMENTS); // User values
+   unsigned int i;                    // Loop index
+   int tmpValue;                      // Placeholder
+   
+   cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
+   for (i = 0; i < revVctr.size(); ++i) {
+      cout << "Value: ";
+      cin >> revVctr.at(i);
+   }
+   
+   // Reverse
+   for (i = 0; i < (revVctr.size() / 2); ++i) {
+      tmpValue = revVctr.at(i);  // These 3 statements swap
+      revVctr.at(i) = revVctr.at(revVctr.size() - 1 - i);
+      revVctr.at(revVctr.size() - 1 - i) = tmpValue;
+   }
+   
+   // Print values
+   cout << endl << "New values: ";
+   for (i = 0; i < revVctr.size(); ++i) {
+      cout << " " << revVctr.at(i);
+   }
+   cout << endl;
+   
+   return 0;
 }
