5c4b681d6d4c1ac2583cd42646cb87035714b85f
bulbabyte <doylemariah18@gmail.com>
2021-08-08 08:50:12 -0500

    Create CarInfo.cpp

---
+++ Classes/CarInfo.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class CarRecord {
+   public:
+      void   SetYearMade(int originalYear);
+      void   SetVehicleIdNum(int vehIdNum);
+      void   Print() const;
+      CarRecord();
+   private:
+      int    yearMade;
+      int    vehicleIdNum;
+};
+
+// FIXME: Write constructor, initialize year to 0, vehicle ID num to -1.
+CarRecord::CarRecord() {
+   yearMade = 0;
+   vehicleIdNum = -1;
+   
+   return;
+}
+/* Your solution goes here  */
+
+void CarRecord::SetYearMade(int originalYear) {
+   yearMade = originalYear;
+   return;
+}
+
+void CarRecord::SetVehicleIdNum(int vehIdNum) {
+   vehicleIdNum = vehIdNum;
+   return;
+}
+
+void CarRecord::Print() const {
+   cout << "Year: " << yearMade << ", VIN: " << vehicleIdNum << endl;
+   return;
+}
+
+int main() {
+   CarRecord familyCar;
+
+   familyCar.Print();
+   familyCar.SetYearMade(2009);
+   familyCar.SetVehicleIdNum(444555666);
+   familyCar.Print();
+
+   return 0;
+}
